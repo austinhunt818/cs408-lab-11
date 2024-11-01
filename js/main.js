3b4a9f11f4dbfa26ed05b603510422cfdc2c637b
Austin Hunt <austinhunt818@u.boisestate.edu>
2024-10-31 18:33:05 -0600

    Main content and some styling done

--- js/main.js
+++ js/main.js
@@ -3,16 +3,67 @@ window.onload = loaded;
 /**
  * Simple Function that will be run when the browser is finished loading.
  */
+
+var xhrResponse = [];
 function loaded() {
-    // Assign to a variable so we can set a breakpoint in the debugger!
-    const hello = sayHello();
-    console.log(hello);
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", function () {
+        xhrResponse = JSON.parse(xhr.response);
+        populateTable(xhrResponse);
+    });
+    xhr.open("GET", "https://va4kva7kjc.execute-api.us-east-2.amazonaws.com/items");
+    xhr.send();
+    
+   for(i=0; i < xhrResponse.length; i++){
+    document.getElementById('tableBody').innerHTML += `<tr><td>Test</td></tr>`;
+   }
 }
 
-/**
- * This function returns the string 'hello'
- * @return {string} the string hello
- */
-export function sayHello() {
-    return 'hello';
+document.getElementById('load-data').onclick = function() {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", function () {
+        xhrResponse = JSON.parse(xhr.response);
+        console.log(xhrResponse);
+        populateTable(xhrResponse);
+    });
+    xhr.open("GET", "https://va4kva7kjc.execute-api.us-east-2.amazonaws.com/items");
+    xhr.send();
 }
+
+function populateTable(tableData){
+    document.getElementById('tableBody').innerHTML = '';
+    for(i=0; i < tableData.length; i++){
+        document.getElementById('tableBody').innerHTML += 
+        `<tr>
+            <td>` + tableData[i].id + `</td>
+            <td>` + tableData[i].name + `</td>
+            <td>` + tableData[i].price + `</td>
+            <td><button onClick=deleteById(` + tableData[i].id + `)>Delete</button></td>
+        </tr>`;
+    }
+}
+
+function deleteById(itemId){
+    let xhr = new XMLHttpRequest();
+    xhr.open("DELETE", "https://va4kva7kjc.execute-api.us-east-2.amazonaws.com/items/" + itemId);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+}
+
+var form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("PUT", "https://va4kva7kjc.execute-api.us-east-2.amazonaws.com/items");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({
+        "id": formData.get('id'),
+        "price": formData.get('price'),
+        "name": formData.get('name'),
+    }));
+    
+})
+
